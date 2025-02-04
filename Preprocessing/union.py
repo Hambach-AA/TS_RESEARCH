c6de73b16afaa0a5c3a42ea07b0e1020082ee60c
Hambach-AA <68084633+Hambach-AA@users.noreply.github.com>
2025-02-04 12:50:35 +0500

    Update union.py

--- Preprocessing/union.py
+++ Preprocessing/union.py
@@ -1,20 +1,20 @@
-def CUDA_Union(name, subseqLen, fragmentLen, fragmentComp, fragmentNum, step):
+def CUDA_Union(name, subseqLen, fragmentNum, fragmentComp, fragmentNum, step):
 
     lib = ctypes.CDLL("C:\\Project\\Preprocessing\\CUDA_TS.dll")
 
     column_names = ["NNdistance_256_1","NNindex_256_1", "NNindexFragment_256_1", "fragment", "num"]
 
-    cTs1 = (ctypes.c_double * (fragmentLen + subseqLen - 1))()
-    cMu1 = (ctypes.c_double * fragmentLen)()
-    cSigma1 = (ctypes.c_double * fragmentLen)()
-    cMp1 = (ctypes.c_double * fragmentLen)()
-    cMpIndex1 = (ctypes.c_int64 * fragmentLen)()
+    cTs1 = (ctypes.c_double * (fragmentNum + subseqLen - 1))()
+    cMu1 = (ctypes.c_double * fragmentNum)()
+    cSigma1 = (ctypes.c_double * fragmentNum)()
+    cMp1 = (ctypes.c_double * fragmentNum)()
+    cMpIndex1 = (ctypes.c_int64 * fragmentNum)()
 
-    cTs2 = (ctypes.c_double * (fragmentLen + subseqLen - 1))()
-    cMu2 = (ctypes.c_double * fragmentLen)()
-    cSigma2 = (ctypes.c_double * fragmentLen)()
-    cMp2 = (ctypes.c_double * fragmentLen)()
-    cMpIndex2 = (ctypes.c_int64 * fragmentLen)()
+    cTs2 = (ctypes.c_double * (fragmentNum + subseqLen - 1))()
+    cMu2 = (ctypes.c_double * fragmentNum)()
+    cSigma2 = (ctypes.c_double * fragmentNum)()
+    cMp2 = (ctypes.c_double * fragmentNum)()
+    cMpIndex2 = (ctypes.c_int64 * fragmentNum)()
 
     cSubseqLen = ctypes.c_int64(subseqLen)
 
@@ -41,33 +41,33 @@ def CUDA_Union(name, subseqLen, fragmentLen, fragmentComp, fragmentNum, step):
             mpIndex2 = client.query_np(f'SELECT NNindex_256_1, NNindexFragment_256_1, fragment, num FROM default.MP_THERMALSENSOR WHERE fragment > {jStepOld} AND fragment < {jFragment} ORDER BY fragment').transpose()
 
             stepTs1 = subseqLen - 1
-            for io in range(0, muSigma1.shape[1], fragmentLen):
+            for io in range(0, muSigma1.shape[1], fragmentNum):
 
-                ctypes.memmove(cTs1, ts1[0][io + stepTs1 - (subseqLen - 1):io + stepTs1 + fragmentLen].ctypes.data, ts1[0][io + stepTs1 - (subseqLen - 1):io + stepTs1 + fragmentLen].nbytes)
-                ctypes.memmove(cMu1, muSigma1[0][io:io + fragmentLen].ctypes.data, muSigma1[0][io:io + fragmentLen].nbytes)
-                ctypes.memmove(cSigma1, muSigma1[1][io:io + fragmentLen].ctypes.data, muSigma1[1][io:io + fragmentLen].nbytes)
-                ctypes.memmove(cMp1, mp1[0][io:io + fragmentLen].ctypes.data, mp1[0][io:io + fragmentLen].nbytes)
-                ctypes.memmove(cMpIndex1, mpIndex1[0][io:io + fragmentLen].ctypes.data, mpIndex1[0][io:io + fragmentLen].nbytes)
+                ctypes.memmove(cTs1, ts1[0][io + stepTs1 - (subseqLen - 1):io + stepTs1 + fragmentNum].ctypes.data, ts1[0][io + stepTs1 - (subseqLen - 1):io + stepTs1 + fragmentNum].nbytes)
+                ctypes.memmove(cMu1, muSigma1[0][io:io + fragmentNum].ctypes.data, muSigma1[0][io:io + fragmentNum].nbytes)
+                ctypes.memmove(cSigma1, muSigma1[1][io:io + fragmentNum].ctypes.data, muSigma1[1][io:io + fragmentNum].nbytes)
+                ctypes.memmove(cMp1, mp1[0][io:io + fragmentNum].ctypes.data, mp1[0][io:io + fragmentNum].nbytes)
+                ctypes.memmove(cMpIndex1, mpIndex1[0][io:io + fragmentNum].ctypes.data, mpIndex1[0][io:io + fragmentNum].nbytes)
 
                 stepTs2 = subseqLen - 1
 
-                for jo in range(0, muSigma2.shape[1], fragmentLen):
+                for jo in range(0, muSigma2.shape[1], fragmentNum):
 
-                    ctypes.memmove(cTs2, ts2[0][jo + stepTs2 - (subseqLen - 1):jo + stepTs2 + fragmentLen].ctypes.data, ts2[0][jo + stepTs2 - (subseqLen - 1):jo + stepTs2 + fragmentLen].nbytes)
-                    ctypes.memmove(cMu2, muSigma2[0][jo:jo + fragmentLen].ctypes.data, muSigma2[0][jo:jo + fragmentLen].nbytes)
-                    ctypes.memmove(cSigma2, muSigma2[1][jo:jo + fragmentLen].ctypes.data, muSigma2[1][jo:jo + fragmentLen].nbytes)
-                    ctypes.memmove(cMp2, mp2[0][jo:jo + fragmentLen].ctypes.data, mp2[0][jo:jo + fragmentLen].nbytes)
-                    ctypes.memmove(cMpIndex2, mpIndex2[0][jo:jo + fragmentLen].ctypes.data, mpIndex2[0][jo:jo + fragmentLen].nbytes)
+                    ctypes.memmove(cTs2, ts2[0][jo + stepTs2 - (subseqLen - 1):jo + stepTs2 + fragmentNum].ctypes.data, ts2[0][jo + stepTs2 - (subseqLen - 1):jo + stepTs2 + fragmentNum].nbytes)
+                    ctypes.memmove(cMu2, muSigma2[0][jo:jo + fragmentNum].ctypes.data, muSigma2[0][jo:jo + fragmentNum].nbytes)
+                    ctypes.memmove(cSigma2, muSigma2[1][jo:jo + fragmentNum].ctypes.data, muSigma2[1][jo:jo + fragmentNum].nbytes)
+                    ctypes.memmove(cMp2, mp2[0][jo:jo + fragmentNum].ctypes.data, mp2[0][jo:jo + fragmentNum].nbytes)
+                    ctypes.memmove(cMpIndex2, mpIndex2[0][jo:jo + fragmentNum].ctypes.data, mpIndex2[0][jo:jo + fragmentNum].nbytes)
 
                     lib.cudaRun_Union(cSubseqLen, cTs1, cMu1, cSigma1, cMp1, cMpIndex1, cTs2, cMu2, cSigma2, cMp2, cMpIndex2)
 
-                    mp2[0][jo:jo + fragmentLen] = np.ctypeslib.as_array(cMp2)
-                    mpIndex2[0][jo:jo + fragmentLen] = np.ctypeslib.as_array(cMpIndex2)
+                    mp2[0][jo:jo + fragmentNum] = np.ctypeslib.as_array(cMp2)
+                    mpIndex2[0][jo:jo + fragmentNum] = np.ctypeslib.as_array(cMpIndex2)
 
                     stepTs2 = stepTs2 + subseqLen -1
 
-                mp1[0][jo:jo + fragmentLen] = np.ctypeslib.as_array(cMp1)
-                mpIndex1[0][jo:jo + fragmentLen] = np.ctypeslib.as_array(cMpIndex1)
+                mp1[0][jo:jo + fragmentNum] = np.ctypeslib.as_array(cMp1)
+                mpIndex1[0][jo:jo + fragmentNum] = np.ctypeslib.as_array(cMpIndex1)
 
                 stepTs1 = stepTs1 + subseqLen -1
 
